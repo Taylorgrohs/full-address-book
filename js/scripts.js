788a7365a963cac689c6089deebd84ebccc5e2be
gracie lundell and taylor grohs <gracielundell@gmail.com>
2016-01-26 08:23:23 -0800

    Pass spec to create full address

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,23 @@ function Contact(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.addresses = [];
-}
+};
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
-}
+};
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+};
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+};
+
+
 
 $(document).ready(function(){
   $("form#new-contact").submit(function(event){
